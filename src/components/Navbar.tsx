56a9681313607d8b602e0f24a59c30f5e060dac9
Lapsus Ordonné <22498591+lapsus-ord@users.noreply.github.com>
2023-06-22 20:00:17 +0200

    fix: add svg for the logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,9 @@
-import { Heading, HStack, Image, Link } from '@chakra-ui/react';
-import { FaGithub } from 'react-icons/fa';
+import { Heading, HStack } from '@chakra-ui/react';
+import { Link as RouterLink } from 'react-router-dom';
 import { css } from '@emotion/react';
 import { NavLink } from '@components/NavLink';
+import { ReactComponent as PaastechLogo } from '@/assets/logo.svg';
 import { LinkType } from '@/typings/link.type';
-import logo from '@/assets/logo.png';
 
 type NavbarProps = {
   links: LinkType[];
@@ -13,32 +13,29 @@ type NavbarProps = {
 
 export const Navbar = (props: NavbarProps) => {
   return (
-    <div css={containerSpace}>
-      <HStack spacing={6} alignItems={'center'}>
-        <Image src={logo} alt={'Logo'} width={50} />
-        <Heading>PaasTech</Heading>
-      </HStack>
+    <div css={containerCss}>
+      <RouterLink to={'/'}>
+        <HStack spacing={4} alignItems={'center'}>
+          <PaastechLogo height={50} />
+          <Heading color={'brand.yellow'}>PaaSTech</Heading>
+        </HStack>
+      </RouterLink>
 
-      <HStack as={'nav'} align={'center'} spacing={6} display={{ base: 'none', md: 'none', lg: 'flex' }}>
+      <HStack as={'nav'} align={'center'} spacing={4} display={{ base: 'none', md: 'none', lg: 'flex' }}>
         {props.links.map((items, i) => (
           <NavLink key={i} link={items.link} title={items.title} />
         ))}
       </HStack>
 
-      <HStack spacing={6} alignItems={'center'}>
+      <HStack spacing={4} alignItems={'center'}>
         <NavLink title={props.secondaryLink.title} link={props.secondaryLink.link} />
-        <NavLink title={props.primaryLink.title} link={props.primaryLink.link} variant={'solid'} bg={'brand.green'} />
-
-        <Link href={'https://github.com/paastech-cloud'} isExternal>
-          <FaGithub size={'50px'} opacity={0.3} />
-        </Link>
+        <NavLink title={props.primaryLink.title} link={props.primaryLink.link} variant={'solid'} bg={'brand.red'} />
       </HStack>
     </div>
   );
 };
 
-const containerSpace = css`
-  margin: 40px 50px 0;
+const containerCss = css`
   display: flex;
   align-items: center;
   justify-content: space-between;
