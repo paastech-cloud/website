ffc3a3d212dbc01049a6b2d19203860fdaa5a834
Lapsus Ordonné <22498591+lapsus-ord@users.noreply.github.com>
2023-06-27 01:15:51 +0200

    feat(dashboard): a simple grid to display projects

---
+++ src/pages/dashboard/DashboardHomePage.tsx
@@ -0,0 +1,37 @@
+import { Box, Card, CardBody, Flex, Heading, HStack, Text } from '@chakra-ui/react';
+import { css } from '@emotion/react';
+
+export const DashboardHomePage = () => {
+  return (
+    <div css={dashboardCss}>
+      <Box as={'aside'} flexBasis={'20rem'} p={'1.5rem'} pl={'2rem'} display={{ base: 'none', lg: 'block' }}></Box>
+
+      <Flex flex={'1'} flexDirection={'column'} bgColor={'white'} border={'1px'} borderColor={'brand.border_gray'}>
+        <HStack justifyContent={'space-between'} borderBottom={'1px'} borderColor={'brand.border_gray'} p={'1.5rem'}>
+          <Heading color={'gray.700'}>Projects</Heading>
+        </HStack>
+        <ul></ul>
+      </Flex>
+
+      <Box as={'aside'} flexBasis={'20rem'} pt={'2rem'} px={'2rem'} display={{ base: 'none', lg: 'block' }}>
+        <Card rounded={'lg'}>
+          <CardBody>
+            <Text fontWeight={'bold'} mb={2}>
+              Welcome to PaaSTech! 🍉
+            </Text>
+            <Text as={'p'}>The simplest PaaS you could ever dream of 💫</Text>
+          </CardBody>
+        </Card>
+      </Box>
+    </div>
+  );
+};
+
+const dashboardCss = css`
+  width: 100%;
+  max-width: 1620px;
+  flex: 1;
+  display: flex;
+  flex-flow: row nowrap;
+  margin: 0 auto;
+`;
