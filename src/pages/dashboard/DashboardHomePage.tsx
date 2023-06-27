323234b23b05e899d45ab7afe865892978a88e4c
Lapsus Ordonné <22498591+lapsus-ord@users.noreply.github.com>
2023-06-27 20:13:38 +0200

    feat(dashboard): breadcrumbs + card

--- src/pages/dashboard/DashboardHomePage.tsx
+++ src/pages/dashboard/DashboardHomePage.tsx
@@ -1,38 +1,61 @@
-import { Box, Card, CardBody, Flex, Heading, HStack, Text } from '@chakra-ui/react';
+import { Card, CardBody, Flex, Heading, HStack, Link, Text, VStack } from '@chakra-ui/react';
+import { ExternalLinkIcon } from '@chakra-ui/icons';
 import { css } from '@emotion/react';
+import { FaGithubAlt } from 'react-icons/fa';
+import { MdSpaceDashboard } from 'react-icons/md';
 import { ProjectList } from '@components/dashboard/ProjectList';
+import { BreadcrumbDashboard } from '@components/dashboard/BreadcrumbDashboard';
 
 export const DashboardHomePage = () => {
   return (
-    <div css={dashboardCss}>
-      <Box as={'aside'} flexBasis={'20rem'} p={'1.5rem'} pl={'2rem'} display={{ base: 'none', lg: 'block' }}></Box>
+    <VStack spacing={8} flex={1} mx={'auto'} pt={8} pb={10} color={'black'} css={maxWidth}>
+      <Flex flexWrap={'nowrap'} flex={'1'} css={maxWidth}>
+        <VStack as={'aside'} flexBasis={'20rem'} p={'1.5rem'} pl={'2rem'} display={{ base: 'none', lg: 'flex' }}></VStack>
 
-      <Flex flex={'1'} flexDirection={'column'} bgColor={'white'} border={'1px'} borderColor={'brand.border_gray'}>
-        <HStack justifyContent={'space-between'} borderBottom={'1px'} borderColor={'brand.border_gray'} p={'1.5rem'}>
-          <Heading color={'gray.700'}>Projects</Heading>
-        </HStack>
-        <ProjectList />
+        <VStack flex={1} spacing={8}>
+          <BreadcrumbDashboard
+            breads={[
+              { title: 'Dashboard', url: '/dashboard', icon: <MdSpaceDashboard /> },
+              { title: 'Projects', isCurrentPage: true },
+            ]}
+          />
+          <Card rounded={'lg'} flex={1} w={'full'} p={8}>
+            <Flex flex={'1'} flexDirection={'column'} bgColor={'white'}>
+              <HStack justifyContent={'space-between'} borderBottom={'1px'} borderColor={'brand.border_gray'} pb={6} pl={4}>
+                <Heading color={'gray.700'}>Projects</Heading>
+              </HStack>
+              <ProjectList />
+            </Flex>
+          </Card>
+        </VStack>
+
+        <VStack spacing={4} alignItems={'stretch'} flexBasis={'20rem'} mt={'35px'} pt={'2rem'} px={'2rem'} display={{ base: 'none', lg: 'flex' }}>
+          <Card rounded={'lg'}>
+            <CardBody>
+              <Text fontWeight={'bold'} mb={2}>
+                Welcome to PaaSTech! 🍉
+              </Text>
+              <Text as={'p'}>The simplest PaaS you could ever dream of 💫</Text>
+            </CardBody>
+          </Card>
+          <Card rounded={'lg'}>
+            <CardBody>
+              <Link href={'https://github.com/paastech-cloud'} isExternal fontWeight={'bold'}>
+                <HStack spacing={2}>
+                  <FaGithubAlt size={'20px'} />
+                  <span>Github</span>
+                  <ExternalLinkIcon />
+                </HStack>
+              </Link>
+            </CardBody>
+          </Card>
+        </VStack>
       </Flex>
-
-      <Box as={'aside'} flexBasis={'20rem'} pt={'2rem'} px={'2rem'} display={{ base: 'none', lg: 'block' }}>
-        <Card rounded={'lg'}>
-          <CardBody>
-            <Text fontWeight={'bold'} mb={2}>
-              Welcome to PaaSTech! 🍉
-            </Text>
-            <Text as={'p'}>The simplest PaaS you could ever dream of 💫</Text>
-          </CardBody>
-        </Card>
-      </Box>
-    </div>
+    </VStack>
   );
 };
 
-const dashboardCss = css`
+const maxWidth = css`
   width: 100%;
   max-width: 1620px;
-  flex: 1;
-  display: flex;
-  flex-flow: row nowrap;
-  margin: 0 auto;
 `;
