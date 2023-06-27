ae64773a2760057892969dc4ea30a92d225066f7
Lapsus Ordonné <22498591+lapsus-ord@users.noreply.github.com>
2023-06-27 20:07:21 +0200

    feat(dashboard): Add resource menu

--- src/components/NavbarLogged.tsx
+++ src/components/NavbarLogged.tsx
@@ -1,7 +1,8 @@
 import { Flex } from '@chakra-ui/react';
 import { LinkType } from '@/typings/link.type';
-import { MenuAccount } from '@components/navbar/MenuAccount';
 import { NavbarLogo } from '@components/navbar/NavbarLogo';
+import { MenuAccount } from '@components/navbar/MenuAccount';
+import { MenuResources } from '@components/navbar/MenuResources';
 
 type NavbarLoggedProps = {
   logoLink: LinkType;
@@ -12,7 +13,8 @@ export const NavbarLogged = (props: NavbarLoggedProps) => {
     <Flex h={16} w={'full'} maxW={'8xl'} justifyContent={'space-between'} marginX={'auto'} px={'2rem'}>
       <NavbarLogo link={props.logoLink} logoWidth={30} fontSize={'1.5rem'} />
 
-      <Flex as={'nav'} alignItems={'center'} justifyContent={'center'}>
+      <Flex as={'nav'} alignItems={'center'} justifyContent={'center'} gap={4}>
+        <MenuResources />
         <MenuAccount />
       </Flex>
     </Flex>
