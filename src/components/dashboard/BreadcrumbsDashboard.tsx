51bdf6aa85fa7f86ca6f29d9b6bab84fae79fa10
Lapsus Ordonné <22498591+lapsus-ord@users.noreply.github.com>
2023-07-02 01:16:24 +0200

    feat(dashboard): add dashboard template

---
+++ src/components/dashboard/BreadcrumbsDashboard.tsx
@@ -0,0 +1,48 @@
+import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, Button, Tooltip } from '@chakra-ui/react';
+import { ChevronRightIcon } from '@chakra-ui/icons';
+import { Link as RouterLink } from 'react-router-dom';
+import { BreadcrumbType } from '@/typings/link.type';
+
+type BreadcrumbDashboardProps = {
+  breadcrumbs: BreadcrumbType[];
+};
+
+export const BreadcrumbsDashboard = ({ breadcrumbs }: BreadcrumbDashboardProps) => {
+  return (
+    <Breadcrumb w={'full'} pl={8} spacing={1} separator={<ChevronRightIcon boxSize={5} color={'gray.500'} />}>
+      {breadcrumbs.map((bread, i) => {
+        const isLastItem = i === breadcrumbs.length - 1;
+
+        const button = (
+          <Button
+            leftIcon={bread.icon}
+            _hover={isLastItem ? {} : undefined}
+            _focus={isLastItem ? {} : undefined}
+            _active={isLastItem ? {} : undefined}
+            cursor={isLastItem ? 'default' : undefined}
+          >
+            {bread.title}
+          </Button>
+        );
+
+        if (isLastItem) {
+          return (
+            <BreadcrumbItem key={i}>
+              <Tooltip label={'You are currently here'} rounded={'md'}>
+                {button}
+              </Tooltip>
+            </BreadcrumbItem>
+          );
+        }
+
+        return (
+          <BreadcrumbItem key={i}>
+            <BreadcrumbLink as={RouterLink} to={bread.url} isCurrentPage={isLastItem}>
+              {button}
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+        );
+      })}
+    </Breadcrumb>
+  );
+};
