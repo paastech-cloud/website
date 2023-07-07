beac05cca14c0b6303a02f6a0daa842058267c97
Lapsus Ordonné <22498591+lapsus-ord@users.noreply.github.com>
2023-07-07 09:48:08 +0200

    feat(dashboard): cards in overview tab component

--- src/pages/dashboard/project-details/OverviewTab.tsx
+++ src/pages/dashboard/project-details/OverviewTab.tsx
@@ -1,25 +1,54 @@
-import { Box, Button, Card, CardBody, Heading, HStack } from '@chakra-ui/react';
+import { Badge, Flex, Button, HStack, Text, Link, VStack, Box } from '@chakra-ui/react';
+import { FaPlay, FaStop } from 'react-icons/fa';
+import { FiRotateCcw } from 'react-icons/fi';
+import { ExternalLinkIcon } from '@chakra-ui/icons';
+import { SimpleCard } from '@components/dashboard/SimpleCard';
 import { ProjectDetailsTabProps } from '@/typings/project-details-tab.type';
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const OverviewTab = (props: ProjectDetailsTabProps) => {
   return (
-    <Box>
-      <Card shadow={'base'}>
-        <CardBody>
-          <Heading as={'h3'} size={'md'} mb={4}>
-            Actions on deploy
-          </Heading>
-
-          <HStack w={'full'} spacing={6}>
-            <Button colorScheme={'green'}>START</Button>
-            <Button colorScheme={'orange'}>STOP</Button>
-            <Button colorScheme={'teal'}>RESTART</Button>
-            <Button colorScheme={'red'}>DELETE</Button>
-          </HStack>
-        </CardBody>
-      </Card>
-      <Box></Box>
-    </Box>
+    <Flex flexDirection={'column'} alignItems={'start'} gap={4}>
+      <SimpleCard heading={'Actions on app'}>
+        <HStack w={'full'} spacing={6}>
+          <Button colorScheme={'green'} leftIcon={<FaPlay size={17} />}>
+            START
+          </Button>
+          <Button colorScheme={'orange'} leftIcon={<FaStop size={17} />}>
+            STOP
+          </Button>
+          <Button colorScheme={'teal'} leftIcon={<FiRotateCcw size={17} />}>
+            RESTART
+          </Button>
+        </HStack>
+      </SimpleCard>
+      <SimpleCard heading={'Application Information'}>
+        <VStack spacing={2} alignItems={'start'}>
+          <Box>
+            <Text as={'p'}>Hostname:</Text>
+            <Link href={`https://${props.project.currentDeployment}.user-app.paastech.cloud`} isExternal color={'brand.bg'} fontWeight={'bold'}>
+              {`${props.project.currentDeployment}.user-app.paastech.cloud`} <ExternalLinkIcon mx={'2px'} />
+            </Link>
+          </Box>
+          <Text as={'p'}>
+            Creation date:{' '}
+            <Badge py={1} px={2} bg={'brand.bg'} color={'white'}>
+              {props.project.createdAt}
+            </Badge>
+          </Text>
+          <Text as={'p'}>
+            Last updated:{' '}
+            <Badge py={1} px={2} bg={'brand.bg'} color={'white'}>
+              {props.project.updatedAt}
+            </Badge>
+          </Text>
+          <Text as={'p'}>
+            Current deployment:{' '}
+            <Badge py={1} px={2} bg={'brand.bg'} color={'white'}>
+              {props.project.currentDeployment}
+            </Badge>
+          </Text>
+        </VStack>
+      </SimpleCard>
+    </Flex>
   );
 };
