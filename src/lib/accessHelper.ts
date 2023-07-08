c156e6d0547f4923c73b7f226274958e49145cc2
DziyanaT <dziyana.tsetserava@etu.umontpellier.fr>
2023-07-09 01:52:47 +0200

    fix: use localStorage instead of sessionStorage

--- src/lib/accessHelper.ts
+++ src/lib/accessHelper.ts
@@ -1,20 +1,20 @@
 import { AccessToken } from '@/api/generated';
 
 export const setAccessToken = (accessToken: AccessToken) => {
-  sessionStorage.setItem('accessToken', accessToken.accessToken);
+  localStorage.setItem('accessToken', accessToken.accessToken);
   const date = new Date();
   // adding 6 hours minus 10 sec
   date.setTime(date.getTime() + 6 * 60 * 60 * 1000 - 10000);
-  sessionStorage.setItem('accessExpiration', date.toISOString());
+  localStorage.setItem('accessExpiration', date.toISOString());
 };
 
 export const getAccessToken = (): AccessToken | null => {
-  const token = sessionStorage.getItem('accessToken');
+  const token = localStorage.getItem('accessToken');
   return !token ? null : { accessToken: token };
 };
 
 const getAccessExpiration = (): Date | null => {
-  const expiryTime = sessionStorage.getItem('accessExpiration');
+  const expiryTime = localStorage.getItem('accessExpiration');
   return !expiryTime ? null : new Date(expiryTime);
 };
 
@@ -24,6 +24,6 @@ export const accessExpired = (): boolean => {
 };
 
 export const removeAccessToken = () => {
-  sessionStorage.removeItem('accessToken');
-  sessionStorage.removeItem('accessExpiration');
+  localStorage.removeItem('accessToken');
+  localStorage.removeItem('accessExpiration');
 };
