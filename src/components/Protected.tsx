b8f25cae4fd4d09e41e1c9abf347137a39ce55b0
Lapsus Ordonné <22498591+lapsus-ord@users.noreply.github.com>
2023-07-10 11:12:48 +0200

    fix(type): remove deprecated type

--- src/components/Protected.tsx
+++ src/components/Protected.tsx
@@ -1,11 +1,15 @@
-import { accessExpired } from '@/lib/accessHelper';
+import { ReactNode } from 'react';
 import { Navigate } from 'react-router-dom';
+import { accessExpired } from '@/lib/accessHelper';
+
 type Props = {
-  children: JSX.Element;
+  children: ReactNode;
 };
-export const Protected = ({ children }: Props): JSX.Element => {
+
+export const Protected = ({ children }: Props) => {
   if (accessExpired()) {
     return <Navigate to={'/login'} replace />;
   }
+
   return children;
 };
