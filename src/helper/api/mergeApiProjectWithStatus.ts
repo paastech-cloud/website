3741fd36b5979a3b45ea4409d7ccb8d66aab1da7
Lapsus Ordonné <22498591+lapsus-ord@users.noreply.github.com>
2023-07-12 01:19:00 +0200

    fix(api): fix merge ApiProject -> Project for ConfigType

--- src/helper/api/mergeApiProjectWithStatus.ts
+++ src/helper/api/mergeApiProjectWithStatus.ts
@@ -1,10 +1,19 @@
 import { ApiProjectStatus, ApiProjectType, ProjectStatus, ProjectType } from '@/typings/project.type';
+import { EnvVariableType } from '@/typings/config.type';
 
 export const mergeApiProjectWithStatus = (project: ApiProjectType, status?: ApiProjectStatus): ProjectType => {
+  let env: EnvVariableType[] = [];
+  if (project.config.env) {
+    // Record<string, string> to Array
+    env = Object.entries(project.config.env)
+      .map(([envKey, value]) => ({ envKey, value }))
+      .sort((a, b) => a.envKey.localeCompare(b.envKey));
+  }
+
   return {
     id: project.id,
     name: project.name,
-    config: project.config,
+    config: { env },
     status: ProjectStatus[status?.container_status ?? 'STATUS_UNKNOWN'],
     updatedAt: project.updatedAt,
     createdAt: project.createdAt,
