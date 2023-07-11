89bbb9ab36762198346536d212328bcd665dc6c7
Dziyana <67862638+sea-gull-diana@users.noreply.github.com>
2023-07-11 13:00:17 +0200

    feat: add Api connection for ssh keys management (#17)
    * feat: add Api connection for ssh keys management
    
    * fix: prettify code with eslint
    
    * feat: use ApiStandardResponse as response type with new openapi.json

--- src/helper/constants.ts
+++ src/helper/constants.ts
@@ -1,6 +1,6 @@
 // Minimum eight characters, at least one uppercase letter,
 // one lowercase letter, one number and one special character
-export const PASSWORD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]$/;
+export const PASSWORD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/;
 
 // starts with a letter, ends with a letter or a figure, contains alphanumerical and hyphens/underscores
 export const USERNAME_REGEX = /^[A-Za-z][A-Za-z0-9_-]+[A-Za-z0-9]$/;
