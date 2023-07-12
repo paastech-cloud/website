b3f910c8abe3c23f5da59eacedf70a07def896b2
Lapsus Ordonné <22498591+lapsus-ord@users.noreply.github.com>
2023-07-12 20:39:10 +0200

    feat(api): action on deploys

--- src/pages/dashboard/project-details/OverviewTab.tsx
+++ src/pages/dashboard/project-details/OverviewTab.tsx
@@ -1,26 +1,51 @@
-import { Badge, Box, Button, Flex, HStack, Link, Text, VStack } from '@chakra-ui/react';
+import { useCallback } from 'react';
+import { Badge, Box, Button, Flex, HStack, Link, Text, useToast, VStack } from '@chakra-ui/react';
 import { FaPlay, FaStop } from 'react-icons/fa';
 import { FiRotateCcw } from 'react-icons/fi';
 import { ExternalLinkIcon } from '@chakra-ui/icons';
 import { SimpleCard } from '@components/dashboard/SimpleCard';
-import { ProjectDetailsTabProps } from '@/typings/project-details-tab.type';
+import { useProjectStore } from '@/stores/project.store';
 import { ProjectStatus } from '@/typings/project.type';
+import { ProjectDetailsTabProps } from '@/typings/project-details-tab.type';
 
 export const OverviewTab = (props: ProjectDetailsTabProps) => {
+  const refresh = useProjectStore((state) => state.refreshCurrentProject);
+  const deploy = useProjectStore((state) => state.deploy);
+  const stop = useProjectStore((state) => state.stop);
+  const toast = useToast();
+
   const canStart = props.project.status === ProjectStatus.STATUS_STOPPED;
   const isRunning = props.project.status === ProjectStatus.STATUS_RUNNING;
+
+  const deployHandle = useCallback(() => {
+    deploy()
+      .then(() => {
+        toast({ title: 'Project deployed successfully ✨', status: 'success', isClosable: true });
+        refresh(props.project.id);
+      })
+      .catch(() => toast({ title: "Error couldn't deploy project...", status: 'error', isClosable: true }));
+  }, [props.project.id]);
+
+  const stopHandle = useCallback(() => {
+    stop()
+      .then(() => {
+        toast({ title: 'Project stopped successfully 👏', status: 'success', isClosable: true });
+        refresh(props.project.id);
+      })
+      .catch(() => toast({ title: "Error couldn't stop the project...", status: 'error', isClosable: true }));
+  }, [props.project.id]);
 
   return (
     <Flex flexDirection={'column'} alignItems={'start'} gap={6}>
       <SimpleCard heading={'Actions on app'}>
         <HStack w={'full'} spacing={6}>
-          <Button colorScheme={'green'} leftIcon={<FaPlay size={17} />} isDisabled={!canStart}>
+          <Button colorScheme={'green'} leftIcon={<FaPlay size={17} />} isDisabled={!canStart} onClick={deployHandle}>
             START
           </Button>
-          <Button colorScheme={'orange'} leftIcon={<FaStop size={17} />} isDisabled={!isRunning}>
+          <Button colorScheme={'orange'} leftIcon={<FaStop size={17} />} isDisabled={!isRunning} onClick={stopHandle}>
             STOP
           </Button>
-          <Button colorScheme={'teal'} leftIcon={<FiRotateCcw size={17} />} isDisabled={!isRunning}>
+          <Button colorScheme={'teal'} leftIcon={<FiRotateCcw size={17} />} isDisabled={!isRunning} onClick={deployHandle}>
             RESTART
           </Button>
         </HStack>
--- src/stores/project.store.ts
+++ src/stores/project.store.ts
@@ -11,6 +11,8 @@ type State = {
 type Action = {
   refreshCurrentProject: (id: string) => Promise<ProjectType>;
   updateConfig: (config: ConfigType) => Promise<void>;
+  deploy: () => Promise<void>;
+  stop: () => Promise<void>;
 };
 
 export const useProjectStore = create<State & Action>((set, get) => ({
@@ -37,5 +39,15 @@ export const useProjectStore = create<State & Action>((set, get) => ({
     set((state) => ({ ...state, currentProject: { ...state.currentProject, config } }));
 
     await projectsApi.projectsControllerDeploy(get().currentProject.id, { env_vars: envVars });
+  },
+  deploy: async () => {
+    const project = get().currentProject;
+    // Array to Record<string, string>
+    const envVars: Record<string, string> = project.config.env?.reduce((result, { envKey, value }) => ({ ...result, [envKey]: value }), {}) ?? {};
+    await projectsApi.projectsControllerDeploy(project.id, { env_vars: envVars });
+  },
+  stop: async () => {
+    const project = get().currentProject;
+    await projectsApi.projectsControllerStop(project.id);
   },
 }));
